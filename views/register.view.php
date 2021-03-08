14e8c9f4d77db3f6f35e6dd95a67f49b764fce57
LuisChavezz <ing.luischavezz@gmail.com>
2021-03-08 00:47:51 -0600

    Contenido y diseño de Register.view

--- views/register.view.php
+++ views/register.view.php
@@ -8,8 +8,33 @@
 
     <link rel="stylesheet" href="css/globalSettings.css">
     <link rel="stylesheet" href="css/register.css">
+
+    <script src="https://kit.fontawesome.com/87f377c3df.js" crossorigin="anonymous"></script>
 </head>
 <body>
-    <h1>HOLA</h1>
+    <div class="content">
+        <h1>Regístrate</h1>
+
+        <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="POST" class="formulario" name="login" autocomplete="off">
+            <div class="form-group">
+                <i class="fas fa-user"></i>
+                <input type="text" name="user" placeholder="User"/>
+            </div>
+
+            <div class="form-group">
+                <i class="fas fa-lock"></i>
+                <input type="password" name="password" placeholder="Password"/>
+            </div>
+
+            <div class="form-group">
+                <i class="fas fa-lock"></i>
+                <input type="password" name="password2" placeholder="Repeat password"/>
+            </div>
+
+            <button onclick="login.submit()">Registrarse</button>
+        </form>
+
+        <p>¿Ya tienes cuenta? <a href="login.php">Iniciar sesión</a></p>
+    </div>
 </body>
 </html>

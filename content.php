65170e703231eb8cd517a0a729748597a8d2555a
LuisChavezz <ing.luischavezz@gmail.com>
2021-03-12 02:05:05 -0600

    Cierre de sesion

--- content.php
+++ content.php
@@ -1,5 +1,10 @@
-<?php
+<?php session_start();
+
+if (isset($_SESSION['user'])) {
+    require 'views/content.view.php';;
+} else {
+    header('Location: login.php');
+}
 
 
-require 'views/content.view.php';
 ?>
--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 if (isset($_SESSION['user'])) {
     header('Location: content.php');
 } else{
-    header('Location: register.php');
+    header('Location: login.php');
 }
 
 ?>
